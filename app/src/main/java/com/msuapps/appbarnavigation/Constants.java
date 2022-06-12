40a4c3709cb6a23190f10eff77dfd420eb027d90
Saifuddin Mohammed <saifmsu15@gmail.com>
2022-06-12 17:26:14 +0530

    This is our 3rd commit.

--- app/src/main/java/com/msuapps/appbarnavigation/Constants.java
+++ app/src/main/java/com/msuapps/appbarnavigation/Constants.java
@@ -3,6 +3,7 @@ package com.msuapps.appbarnavigation;
 public class Constants {
     public static final String baseUrl = "https://www.google.com";
 
+    public static final String anotherBaseUrl = "https://www.instagram.com";
 
 
 }
