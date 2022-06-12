1f485c059cdb107340b6cde275410f0c23489edf
Saifuddin Mohammed <saifmsu15@gmail.com>
2022-06-13 00:16:47 +0530

    change 4 commit

--- app/src/main/java/com/msuapps/appbarnavigation/Constants.java
+++ app/src/main/java/com/msuapps/appbarnavigation/Constants.java
@@ -3,10 +3,6 @@ package com.msuapps.appbarnavigation;
 public class Constants {
     public static final String baseUrl = "https://www.google.com";
     public static final String anotherBaseUrl = "https://www.instagram.com";
-
     public static final String experimentalUrl = "change 4";
-
     public static final String lastUrl = "This line will be Merged";
-
-
 }
