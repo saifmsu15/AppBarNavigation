c02974ad20f0acfaadf0dddfa8b7047b29f3b123
Saifuddin Mohammed <saifmsu15@gmail.com>
2022-06-13 22:10:55 +0530

    change 12 commit.

--- app/src/main/java/com/msuapps/appbarnavigation/Constants.java
+++ app/src/main/java/com/msuapps/appbarnavigation/Constants.java
@@ -7,4 +7,7 @@ public class Constants {
     public static final String lastUrl = "This line will be Merged";
     public static final String fromFriendUrl = "Hello from your friend";
     public static final String fromMyselfUrl = "Hello from Myself";
+
+
+    asad uddin owaisi is a great leader
 }
